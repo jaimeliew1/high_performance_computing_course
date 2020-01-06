c39704a7b5f838f2233f9899969c0536c3fc79d8
Jaime Liew <jyli@dtu.dk>
2020-01-06 14:23:56 +0100

    Add heat equation code for day 1

---
+++ week1/ex1a.f90
@@ -0,0 +1,12 @@
+PROGRAM test
+  IMPLICIT NONE
+  INTEGER :: i,j,k,n
+  PRINT*,'Enter j n'
+  READ(*,*) j,n
+  DO i=1,n
+     IF (i.EQ.j) EXIT
+     !i = i + 10 ! does this compile ?
+  ENDDO
+  k = i
+  PRINT*,'k = ',k
+END PROGRAM test
