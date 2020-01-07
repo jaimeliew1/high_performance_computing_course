7391dc7f849bc997d5752840c9e0af67fe8c3bc2
Jaime Liew <jyli@dtu.dk>
2020-01-07 10:56:07 +0100

    add ex2A - interfaces

---
+++ week1/ex2a.f90
@@ -0,0 +1,43 @@
+PROGRAM main
+  REAL, DIMENSION ( : ) , POINTER :: my_data
+  INTEGER :: n, status
+
+  INTERFACE
+     SUBROUTINE sub (DATA, n, info)
+       REAL, DIMENSION ( : ) , POINTER :: DATA
+     END SUBROUTINE sub
+     SUBROUTINE init (DATA, n, info)
+       REAL, DIMENSION(*) :: DATA
+     END SUBROUTINE
+  END INTERFACE
+
+  n = 12
+  CALL sub (my_data, n, status)
+  PRINT* , ' status = ', status, ASSOCIATED(my_data)
+
+  CALL init(my_data, n, status)
+  DO i=1,n
+     PRINT* , 'my_data (i) = ' , my_data(i)
+  ENDDO
+END PROGRAM main
+
+
+
+SUBROUTINE sub (DATA, n, info)
+  REAL, DIMENSION ( : ) , POINTER :: DATA
+  INTEGER :: n, info
+
+  ALLOCATE (DATA (n) , stat=info)
+END SUBROUTINE sub
+
+
+
+
+SUBROUTINE init (DATA, n, info)
+  REAL, DIMENSION(*) :: DATA
+
+  INTEGER :: n, info
+  DO i=1, n
+    DATA(i) = -i
+  ENDDO
+END SUBROUTINE init
