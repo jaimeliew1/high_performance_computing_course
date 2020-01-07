70021aae65913730708fb96bdfea5780d28bb7a8
Jaime Liew <jyli@dtu.dk>
2020-01-07 14:19:41 +0100

    Add python animation.

---
+++ week1/heat_equation/animate.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
+import numpy as np
+
+def make_frame(t):
+    filename = f'diff{make_frame.counter:06d}.dat'
+
+    A = np.loadtxt(filename)
+    x, y, T = A[:, 0].reshape((21,21)), A[:, 1].reshape((21,21)), A[:, 2].reshape((21,21))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_wireframe(x, y, T)
+    ax.set_zlim(0, 1)
+
+    img = mplfig_to_npimage(fig)
+    plt.close(fig)
+
+    make_frame.counter += 1
+    return img
+make_frame.counter = 1
+
+
+fps = 20
+N = 200
+duration = (N-1)/fps
+
+animation = VideoClip(make_frame, duration=duration)
+animation.write_gif('gif.gif', fps=fps)
