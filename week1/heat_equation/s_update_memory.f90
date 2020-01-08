6d44bf840a9803a5bf62535794a16fd4f61fdfe9
Jaime Liew <jaimeliew1@gmail.com>
2020-01-08 14:15:01 +0100

    heat equation - add an ELEMENTAL subroutine

--- week1/heat_equation/s_update_memory.f90
+++ week1/heat_equation/s_update_memory.f90
@@ -1,4 +1,4 @@
-SUBROUTINE update_memory(A, A_old)
-  REAL, DIMENSION(:, :), INTENT(inout) :: A, A_old
+ELEMENTAL SUBROUTINE update_memory(A, A_old)
+  REAL, INTENT(inout) :: A, A_old
   A_old = A
 END SUBROUTINE update_memory
